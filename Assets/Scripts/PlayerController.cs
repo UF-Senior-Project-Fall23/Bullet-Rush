48497189a8a84ca90767922a168c6f8ed5268fb4
RodolfoDA22 <rodolfodearmasjr@gmail.com>
2023-10-24 21:07:56 -0400

    Created Dashing and Invulnerability Functionality
    The iFrames work, but for the dash I'm trying to figure out how to make it so that it goes in a certain direction and you can do it after spawning.
    
    I'm pretty sure my code is also supposed to change the opacity but for now it changes the players color. :(

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,6 +13,7 @@ public class PlayerController : MonoBehaviour
     float m_vertical;
     Vector3 m_zero = Vector3.zero;
     bool m_alive = true;
+    [SerializeField] private TrailRenderer trail;
 
     GameObject weapon;
     weaponScript m_weaponScript;
@@ -32,9 +33,25 @@ public class PlayerController : MonoBehaviour
     //Player current health
     public int currHealth { get; private set; }
 
+    //iFrames Duration
+    [Header("iFrames")]
+    [SerializeField] private float iFramesDuration;
+
+    //Flashes that occur before the player gets out of iFrames
+    [SerializeField] private int numberOfFlashes;
+
+    private SpriteRenderer spriteOpacity;
+
+    private bool canDash = true;
+    private bool isDashing;
+    private float dashingPower = 24f;
+    private float dashingTime = 0.2f;
+    private float dashingCooldown = 0.2f;
+
     private void Awake()
     {
         m_body = GetComponent<Rigidbody2D>();
+        spriteOpacity = GetComponent<SpriteRenderer>();
         currHealth = maxHealth;
         if (instance == null)
         {
@@ -48,11 +65,18 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-
+        //In order to not have the player do any other movements if it is currently dashing we need to add this code block
+        if(isDashing) {
+            return;
+        }
     }
 
     void FixedUpdate()
     {
+        if(isDashing) {
+            return;
+        }
+
         if (m_alive)
         {
             //Gets raw movement input
@@ -72,6 +96,9 @@ public class PlayerController : MonoBehaviour
             m_vertical *= moveLimiter;
         }
 
+        if(Input.GetKeyDown(KeyCode.Space) && canDash) {
+            StartCoroutine(Dash());
+        }
 
         // This finds the target velocity of the player
         Vector3 targetVelocity = new Vector2(m_horizontal * runSpeed, m_vertical * runSpeed);
@@ -86,6 +113,7 @@ public class PlayerController : MonoBehaviour
         if (collision.gameObject.tag == "Enemy")
         {
             decreaseHealth(1);
+            StartCoroutine(Invulnerability());
         }
         //Pick up weapon only if player doesnt have a weapon
         else if (collision.gameObject.tag == "Weapon" && weapon == null)
@@ -120,6 +148,42 @@ public class PlayerController : MonoBehaviour
             currHealth = 0;
             m_alive = false;
         }
+    }
+
+    private IEnumerator Invulnerability() {
+        Physics2D.IgnoreLayerCollision(7, 8, true);
+        //invulnerability duration
+        for(int i = 0; i < numberOfFlashes; i++){
+            spriteOpacity.color = new Color(255, 0, 0, 20);
+            yield return new WaitForSeconds(iFramesDuration / (numberOfFlashes * 2));
+            spriteOpacity.color = new Color(255, 255, 255, 255);
+            yield return new WaitForSeconds(iFramesDuration / (numberOfFlashes * 2));
+        }
+        Physics2D.IgnoreLayerCollision(7, 8, false);
+    }
+
+    private IEnumerator Dash() {
+        canDash = false;
+        isDashing = true;
+        StartCoroutine(Invulnerability());
+        float originalGravity = m_body.gravityScale;
+        m_body.gravityScale = 0f;
+        m_horizontal = Input.GetAxisRaw("Horizontal");
+        m_vertical = Input.GetAxisRaw("Vertical");
+        if (m_horizontal != 0 && m_vertical != 0)
+        {
+            m_horizontal *= moveLimiter;
+            m_vertical *= moveLimiter;
+        }
+        // Vector2(xVelocity, yVelocity) in this case yVelocity is 0, might have to adjust this for isometric character
+        m_body.velocity = new Vector2(m_horizontal * dashingPower, m_vertical * dashingPower);
+        trail.emitting = true;
+        yield return new WaitForSeconds(dashingTime);
+        trail.emitting = false;
+        m_body.gravityScale = originalGravity;
+        isDashing = false;
+        yield return new WaitForSeconds(dashingCooldown);
+        canDash = true;
     }
 
 }
