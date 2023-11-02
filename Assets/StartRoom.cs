3d0709ffd0cc1177fd41ac32f89a17ace646fafa
nathankrasno <nakrasno@gmail.com>
2023-11-02 13:46:50 -0400

    Added tutorial images for WASD movement

--- Assets/StartRoom.cs
+++ Assets/StartRoom.cs
@@ -2,12 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 
 public class StartRoom : MonoBehaviour
 {
     public TextMeshProUGUI tutorialText;
     private GameObject player;
+    public Image wImage;
+    public Image aImage;
+    public Image sImage;
+    public Image dImage;
+    public Sprite greenWImageSprite;
+    public Sprite greenAImageSprite;
+    public Sprite greenSImageSprite;
+    public Sprite greenDImageSprite;
+    public Sprite grayWImageSprite;
+    public Sprite grayAImageSprite;
+    public Sprite graySImageSprite;
+    public Sprite grayDImageSprite;
+
 
     // Start is called before the first frame update
     public List<GameObject> tutorialSquares;
@@ -24,28 +38,38 @@ public class StartRoom : MonoBehaviour
         }
     }
 
+    void Update()
+    {
+        changeColorOnKeyPress();
+    }
+
     // Update is called once per frame
     void FixedUpdate()
     {
         if (IsPlayerInsideSquare(player.transform.position, tutorialSquares[0]))
         {
             tutorialText.text = "Walk into the weapon to pick it up.";
+            disableImages();
         }
         else if (IsPlayerInsideSquare(player.transform.position, tutorialSquares[1]))
         {
             tutorialText.text = "Use \nWASD\nto move.";
+            enableImages();
         }
         else if (IsPlayerInsideSquare(player.transform.position, tutorialSquares[2]))
         {
             tutorialText.text = "Select Easy, Medium, or Hard";
+            disableImages();
         }
         else if (IsPlayerInsideSquare(player.transform.position, tutorialSquares[3]))
         {
             tutorialText.text = "Enter Here to Start";
+            disableImages();
         }
         else
         {
             tutorialText.text = "";
+            disableImages();
         }
     }
 
@@ -60,6 +84,62 @@ public class StartRoom : MonoBehaviour
         {
             Debug.LogWarning("Square does not have a BoxCollider2D component.");
             return false;
+        }
+    }
+
+    void disableImages()
+    {
+        wImage.enabled = false;
+        aImage.enabled = false;
+        sImage.enabled = false;
+        dImage.enabled = false;
+        //set to gray
+        wImage.sprite = grayWImageSprite;
+        aImage.sprite = grayAImageSprite;
+        sImage.sprite = graySImageSprite;
+        dImage.sprite = grayDImageSprite;
+    }
+    void enableImages()
+    {
+        wImage.enabled = true;
+        aImage.enabled = true;
+        sImage.enabled = true;
+        dImage.enabled = true;
+    }
+    void changeColorOnKeyPress()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            wImage.sprite = greenWImageSprite; // You can specify the green sprite you want to use.
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            aImage.sprite = greenAImageSprite; // You can specify the green sprite you want to use.
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            sImage.sprite = greenSImageSprite; // You can specify the green sprite you want to use.
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            dImage.sprite = greenDImageSprite; // You can specify the green sprite you want to use.
+        }
+
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            wImage.sprite = grayWImageSprite;
+        }
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            aImage.sprite = grayAImageSprite;
+        }
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            sImage.sprite = graySImageSprite;
+        }
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            dImage.sprite = grayDImageSprite;
         }
     }
 }
